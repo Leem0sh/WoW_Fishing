2ac99b15fa44cbed9984253fd467d954aff52a25
Leem0sh <vrt2@seznam.cz>
2020-12-14 01:13:23 +0100

    Bait deploy,  bait options, documentation needed + clean code

--- src/fishing_v2.py
+++ src/fishing_v2.py
@@ -15,6 +15,13 @@ import sys
 from tkinter import *
 import datetime
 from PIL import ImageGrab
+import json
+
+
+def press_key(key):
+    pyautogui.keyDown(key)
+    time.sleep(0.1)
+    pyautogui.keyUp(key)
 
 
 class Application:
@@ -228,7 +235,7 @@ class Fishing:
     def send_float(self):
         pyautogui.press(self.config.get("Settings", "Button"))
         print("Button hit, waiting for animation")
-        time.sleep(2.5)
+        time.sleep(3)
 
     # def make_screenshot(self):
     #     print("Capturing screen")
@@ -241,6 +248,31 @@ class Fishing:
     #     #     screenshot_name = '.\\var\\fishing_session.png'
     #     # screenshot.save(screenshot_name)
     #     return screenshot
+
+    ##### 1300 250 1875 945
+
+    def bait_deploy(self, fish):
+        pyautogui.moveTo(1, 1, 0)
+        press_key("b")
+        loc = pyautogui.locateOnScreen(
+            os.path.join(__file__, f"..\..\icons\\{fish.lower()}.png"),
+            confidence=0.8,
+            region=(1300, 250, 1875, 945),
+        )
+        if loc != None:
+            center_of_icon = (loc[0] + (loc[2] / 2), loc[1] + (loc[3] / 2))
+            self.move_mouse(center_of_icon)
+            pyautogui.click(button="right")
+            time.sleep(0.1)
+            press_key("b")
+
+            global bait_timer
+            bait_timer = time.time()
+            global bait_in_inventory
+            bait_in_inventory = True
+        else:
+            # No bait in inventory
+            bait_in_inventory = False
 
     def find_float(self):
 
@@ -321,55 +353,107 @@ class Fishing:
         )
 
     def snatch(self):
-        print("Snatching!")
-        time.sleep(random.uniform(0.2, 1))
+        time.sleep(random.uniform(0.2, 0.4))
         pyautogui.click()
+
+
+def logWriter(start_time, catched, tries, wantcatch):
+    data = {
+        time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time)): {
+            "EndTime": datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+            "Catched": catched,
+            "Tries": tries,
+            "SuccessRate": round((catched / tries) * 100, 2),
+            "Took": round((time.time() - start_time) / 60, 2),
+            "WantedToCatch": wantcatch,
+        }
+    }
+    with open(
+        os.path.join(__file__, "..\..\catchlog.log"), "a+", encoding="utf-8"
+    ) as f:
+
+        json.dump(data, f, indent=6)
 
 
 @click.command()
 @click.option(
     "--wantcatch",
-    "-wc",
-    prompt="How many fish do you want to capture?",
-    help="While number of captured fish is lower than the number you want to capture, it's gonna be fishing",
+    "-c",
+    prompt="How many fish do you want to catch?",
+    help="While number of catched fish is lower than the number you want to catch, it's gonna be fishing",
+    type=click.INT,
 )
-def main(wantcatch):
-    s = Fishing()
-    if s.config.get("Settings", "Device_check") == "on":
-        s.device_check()
-    else:
-        pass
-    ###### CAPTURE SCREEN #######
-    root = Tk()
-    app = Application(root)
-    root.mainloop()
-    ###### CAPTURE SCREEN #######
+@click.option(
+    "--printscreen", "-p", is_flag=True, help="Allowing recreating printscreen"
+)
+@click.option(
+    "--bait",
+    "-b",
+    help="Uses bait before starting the session and inside session Usage: -b amberjack = Iridescent Amberjack / piranha = Spinefin Piranha / pike = Silvergrill Pike / bonefish = Pocked Bonefish ",
+    type=click.Choice(["amberjack", "piranha", "pike", "bonefish"]),
+)
+def main(wantcatch, printscreen, bait):
+    try:
+        print(bait)
+        s = Fishing()
+        if s.config.get("Settings", "Device_check") == "on":
+            s.device_check()
+        else:
+            pass
+        ###### CAPTURE SCREEN #######
+        if printscreen:
+            root = Tk()
+            app = Application(root)
+            root.mainloop()
+        ###### CAPTURE SCREEN #######
 
-    proc_id = s.get_proc_id()
-    hwnd = s._get_hwnd_by_pid(proc_id)
-    s.check_screen_size(hwnd)
+        proc_id = s.get_proc_id()
+        hwnd = s._get_hwnd_by_pid(proc_id)
+        s.check_screen_size(hwnd)
 
-    tries = 0
-    while s.catched < int(wantcatch):
-        tries += 1
-        s.send_float()
-        # image = s.make_screenshot()
-        place = s.find_float()
-        if not place:
-            print("Bobber not found")
-            continue
-        print("Bobber found at " + str(place))
-        s.move_mouse(place)
-        if not s.listen():
-            print("If we didn' hear anything, lets try again")
+        tries = 0
+        ### FIRST BAIT SECTION ###
+        if bait:
+            # FIRST BAIT FUNCTION
+            s.bait_deploy(bait)
+
+            pass
+
+        #########################
+
+        while s.catched < wantcatch:
+            now = time.time()
+            if bait and bait_in_inventory:
+                if now - bait_timer > 1800:
+                    s.bait_deploy(bait)
+            # print("BAIT CHECK")
+            # s.first_bait_check()
+            tries += 1
+            s.send_float()
+            # image = s.make_screenshot()
+            place = s.find_float()
+            if not place:
+                print("Bobber not found")
+                continue
+            print("Bobber found at " + str(place))
+            s.move_mouse(place)
+            if not s.listen():
+                print("If we didn' hear anything, lets try again")
+                time.sleep(1)
+                continue
+            s.snatch()
+            s.timing()
+            time.sleep(random.uniform(0.8, 1.1))
+            s.catched += 1
+            print("guess we've snatched something")
+            print("catched " + str(s.catched))
+            print(
+                f"Succesful {s.catched} / {tries} | {round((s.catched / tries)*100, 2)}%"
+            )
             time.sleep(1)
-            continue
-        s.snatch()
-        s.timing()
-        time.sleep(random.uniform(0.8, 1.1))
-        s.catched += 1
-        print("guess we've snatched something")
-        print("catched " + str(s.catched))
-        print(f"Succesful {s.catched} / {tries} | {round((s.catched / tries)*100, 2)}%")
-    s.p.terminate()
-    print("We done!")
+        s.p.terminate()
+        logWriter(s.start_time, s.catched, tries, wantcatch)
+
+        print("We done!")
+    except KeyboardInterrupt:
+        logWriter(s.start_time, s.catched, tries, wantcatch)
--- testing/test.py
+++ testing/test.py
@@ -1,73 +1,13 @@
-from numpy import where, array
-import cv2
-import pyautogui
-import pyaudio
-import audioop
-import os
 import time
-import psutil
-import random
-import configparser
-import win32gui
-import win32process
-import sys
-from PIL import ImageGrab
-from PyQt5 import QtWidgets, QtCore, QtGui
-import tkinter as tk
-from PIL import ImageGrab
 
-finimg = None
+bait_time = time.time()
+for x in range(10):
 
+    time.sleep(1.6)
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        root = tk.Tk()
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
-        self.setGeometry(0, 0, screen_width, screen_height)
-        self.setWindowTitle(" ")
-        self.begin = QtCore.QPoint()
-        self.end = QtCore.QPoint()
-        self.setWindowOpacity(0.3)
-        QtWidgets.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.CrossCursor))
-        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-        print("Capture the screen...")
-        self.show()
+    now = time.time()
 
-    def paintEvent(self, event):
-        qp = QtGui.QPainter(self)
-        qp.setPen(QtGui.QPen(QtGui.QColor("black"), 3))
-        qp.setBrush(QtGui.QColor(128, 128, 255, 128))
-        qp.drawRect(QtCore.QRect(self.begin, self.end))
-
-    def mousePressEvent(self, event):
-        self.begin = event.pos()
-        self.end = self.begin
-        self.update()
-
-    def mouseMoveEvent(self, event):
-        self.end = event.pos()
-        self.update()
-
-    def mouseReleaseEvent(self, event):
-        self.close()
-
-        x1 = min(self.begin.x(), self.end.x())
-        y1 = min(self.begin.y(), self.end.y())
-        x2 = max(self.begin.x(), self.end.x())
-        y2 = max(self.begin.y(), self.end.y())
-
-        img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
-        img.save("capture.png")
-        finimg = cv2.cvtColor(array(img), cv2.COLOR_BGR2RGB)
-
-        cv2.imshow("Captured Image", finimg)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-
-app = QtWidgets.QApplication(sys.argv)
-window = MyWidget()
-window.show()
-time.sleep(100)
+    if now - bait_time < 10:
+        print(x, now, bait_time, now - bait_time)
+    else:
+        print(now, bait_time, now - bait_time)
